185b2f641fe5b7526d429faa90307e4ff2f772c5
Listopad Bogdan <listopad.bogdan@bk.ru>
2022-04-01 13:27:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+from fastapi import FastAPI, Response
+
+from typing import Optional
+
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def hello_page(name: Optional[str] = '', message: Optional[str] = ''):
+    return Response(
+        f'Hello, {name}! '
+        f'{message}!', media_type='text/html'
+    )
+
+
+if __name__ == '__main__':
+    print('Запускаем сервер!')
+    uvicorn.run('main:app', reload=True)
+    
